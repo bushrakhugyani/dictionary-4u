41cc4da4b5b2b2369cb1a4301d3954b8383d0aff
Bushra Khugyani <160358779+bushrakhugyani@users.noreply.github.com>
2024-05-14 20:21:42 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
+import Dictionary from "./Dictionary.js";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       English Dictionary
+     English Dictionary
       </header>
+      <main>
+        < Dictionary />
+      </main>
+      <footer className="App-footer">
+        Coded by Bushra Khugyani
+      </footer>
     </div>
   );
 }
