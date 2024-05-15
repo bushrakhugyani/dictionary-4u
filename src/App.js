876cc53c6811425be082739ddfe315cc4e3d54ed
Bushra Khugyani <160358779+bushrakhugyani@users.noreply.github.com>
2024-05-14 18:40:30 -0700

    fixed header typo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       Englidh Dictionary
+       English Dictionary
       </header>
     </div>
   );
